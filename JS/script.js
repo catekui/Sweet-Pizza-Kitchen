1e05fcc49f03d0e93ec48ce8449b4352f8f3baa2
catekui <catherine.wanjiku@student.moringaschool.com>
2022-03-20 17:48:04 +0300

    refactor the code

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,12 @@
+function Order(pizza, toppings, crust){
+    this.pizza=pizza;
+    this.toppings=toppings;
+    this.crust=crust;
+}
 $(document).ready(function(){
-    // constructor
-    function Order(pizza, toppings, crust){
-        this.pizza=pizza;
-        this.toppings=toppings;
-        this.crust=crust;
-    }
+    
 
-    $("button").click(function(){
+    $("#button").click(function(){
         function myPizza(){
             const pizzaCost = document.getElementById("pizza").value;
             return parseInt(pizzaCost);
@@ -21,8 +21,12 @@ $(document).ready(function(){
         }
         let cost =  new Order(myPizza(), myToppings(), myCrust())
         
-        var totalCost = cost.pizza + cost.toppings + cost.crust
-        alert(totalCost)
+        var totalCost = cost.pizza + cost.toppings + cost.crust 
+        // alert(totalCost)
+    
+       
+        $(".pizza1").append(('<tr><td class="size">'+ cost.pizza +'</td><td class="toppings">' + cost.toppings + '</td><td class="crust">'+ cost.crust + '</td><td class="total">'+ totalCost))
+       
 
 
         
