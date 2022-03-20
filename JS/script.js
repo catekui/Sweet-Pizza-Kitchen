9ec2d6c20364c545cddb32c35b9d07442ee34da0
catekui <catherine.wanjiku@student.moringaschool.com>
2022-03-20 12:19:21 +0300

    refactor the code

--- JS/script.js
+++ JS/script.js
@@ -5,5 +5,21 @@ $(document).ready(function(){
         this.toppings=toppings;
         this.crust=crust;
     }
-    
+    $("button").click(function(){
+        function myPizza(){
+            const pizza = document.getElementById("pizza").ariaValueMax;
+            parseInt(pizza);
+        }
+        function myToppings(){
+            const toppings = document.getElementById("toppings").ariaValueMax;
+            parseInt(toppings);
+        }
+        function myCrust(){
+            const crust = document.getElementById("crust").ariaValueMax;
+            parseInt(crust);
+        }
+        if (pizza === "" ||toppings=== "" || crust === ""){
+            alert("Please choose something")
+        }
+    })
 })
